7440931ac71e3d58983b71559701d2e5cd888639
Ray Dehler <rdehler@fb.com>
2014-02-27 14:20:14 -0800

    POC: pull down badges for a user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
+var options = {
+  host: 'api.untappd.com',
+  port: 80,
+  path: '/v4/user/badges/rdehler?client_id='+process.env.CLIENT_ID+'&client_secret='+process.env.CLIENT_SECRET,
+  method: 'GET'
+};
 
-/*
- * GET home page.
- */
+var http = require('http');
+var json = '';
+
+http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    json += chunk.replace(/\\"/g, "'");
+  });
+
+}).end();
+
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { json: json });
 };
