dd1de39a49bce8af8f88c55210b26752423b0fa0
Ray Dehler <rdehler@fb.com>
2014-02-27 11:47:41 -0800

    testing with api

--- web.js
+++ web.js
@@ -4,7 +4,22 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
+var https = require("http");
+var optionsget = {
+host: 'api.untappd.com',
+port: '80'
+path: '/v4/user/info/rdehler?client_id=DE2486E62F8B6DFBF0F5089CCE0EEBD09DBEFAB5&client_secret=8804D03E2BA53910C5E02AFBC051D100943387F4',
+method: 'GET'
+};
+
+
 app.get('/', function(req, res) {
+  http.request(optionsget, function(res2) {
+    res2.setEncoding('utf8');
+    res2.on('data', function(chunk) {
+      res.send(chunk);
+    });
+  });
   res.send('Hello World! (2)');
 });
 
